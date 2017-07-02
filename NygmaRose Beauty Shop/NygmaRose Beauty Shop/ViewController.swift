66850f853340dfeae08362291c30539aa911d617
Musa Fletcher <musafletcher@Musas-Mac.local>
2017-07-02 23:04:24 +1000

    Initial Xcode Single View Application

---
+++ NygmaRose Beauty Shop/NygmaRose Beauty Shop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NygmaRose Beauty Shop
+//
+//  Created by Musa Fletcher on 2/7/17.
+//  Copyright © 2017 NygmaRose Beauty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ NygmaRose Beauty Shop/NygmaRose Beauty ShopTests/NygmaRose_Beauty_ShopTests.swift
@@ -0,0 +1,36 @@
+//
+//  NygmaRose_Beauty_ShopTests.swift
+//  NygmaRose Beauty ShopTests
+//
+//  Created by Musa Fletcher on 2/7/17.
+//  Copyright © 2017 NygmaRose Beauty. All rights reserved.
+//
+
+import XCTest
+@testable import NygmaRose_Beauty_Shop
+
+class NygmaRose_Beauty_ShopTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
