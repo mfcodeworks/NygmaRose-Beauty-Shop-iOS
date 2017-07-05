3de80c47bdb70bf011587baedb71bb1db71c6b5a
Musa Fletcher <musafletcher@Musas-Mac.local>
2017-07-05 22:32:09 +1000

    Stable WebView Load

--- NygmaRose Beauty Shop/NygmaRose Beauty Shop/ViewController.swift
+++ NygmaRose Beauty Shop/NygmaRose Beauty Shop/ViewController.swift
@@ -1,4 +1,4 @@
-//
+	//
 //  ViewController.swift
 //  NygmaRose Beauty Shop
 //
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate {
+    // Define Views
+    var webView: WKWebView!
+    var progressView: UIProgressView!
+    
+    override func loadView() {
+        // Load Initial WebView
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Create Progress View
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        toolbarItems = [progressButton]
+        navigationController?.isToolbarHidden = false
+        //Set and Load Initial URL
+        let url = URL(string: "https://shop.nygmarose.com/")!
+        webView.load(URLRequest(url: url))
+        // Set WebView Config
+        webView.allowsBackForwardNavigationGestures = true
+        webView.scrollView.isScrollEnabled = true
+        webView.scrollView.bounces = true
+        // Allow Scroll to Refresh
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(ViewController.refreshWebView), for: UIControlEvents.valueChanged)
+        webView.scrollView.addSubview(refreshControl)
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
     }
-
+    
+    func refreshWebView() {
+        // On Scroll to Refresh, Reload Current Page
+        webView.reload()
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        // Display Progress Bar While Loading Pages
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
